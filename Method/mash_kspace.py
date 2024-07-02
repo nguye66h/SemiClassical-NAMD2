468025714b96b5060a74a036737bd9a6768e892b
nguye66h <nguye66h@mtholyoke.edu>
2024-07-02 12:02:22 -0500

    fix the delta_k bug, previous commit shows mash.py with the bug

--- Method/mash_kspace.py
+++ Method/mash_kspace.py
@@ -25,14 +25,14 @@ class Bunch:
 
 
 
-def initElectronic(Nstates, initState, Hij):
-
-    k = np.arange(0,2*np.pi,2*np.pi/(Nstates))
+def initElectronic(Nstates, initState, Hij, k):
     
     # while(True):
     #     x = np.random.normal(size=Nstates)
     #     y = np.random.normal(size=Nstates)
     #     if (np.argmax(x**2 + y**2) == initState):
+    #         np.savetxt('x.txt', x)
+    #         np.savetxt('y.txt', y)
     #         break
 
     x = np.loadtxt('x.txt')
@@ -42,48 +42,48 @@ def initElectronic(Nstates, initState, Hij):
     ck = (x+1j*y)/norm
     phasek = np.angle(ck[initState])
     ck = ck*np.exp(-1j*phasek)
-    cn = np.einsum('kn,k->n',np.exp(np.outer(-1j*k,np.arange(Nstates))),ck)/np.sqrt(Nstates)
-
     _, U = np.linalg.eigh(Hij)
-    c = np.conj(U).T @ cn
+    c = np.conj(U).T @ ck
     return c # in adiabatic basis
  
-def Force(dHij, dH0, acst, U):
-
+def Force_on_p(dHij_dq, acst, U):
     # dHij is in the diabatic basis !IMPORTANT
-    F = -dH0
     # <a |dH | a> -->   ∑ij <a | i><i | dH |j><j| a>
-    F -= np.einsum('j, ijk, k -> i', U[:, acst].conjugate(), dHij + 0j, U[:,acst]).real
-
+    F = np.einsum('j, ijk, k -> i', U[:, acst].conjugate(), dHij_dq + 0j, U[:,acst]).real
     return F
+
+def Force_on_q(dHij_dp, acst, U):
+    # dHij is in the diabatic basis !IMPORTANT
+    # <a |dH | a> -->   ∑ij <a | i><i | dH |j><j| a>
+    F = np.einsum('j, ijk, k -> i', U[:, acst].conjugate(), dHij_dp + 0j, U[:,acst]).real
+    return F 
 
 def VelVer(ogdat, acst, dt) : 
 
     dat = copy.deepcopy(ogdat)
 
-    # print("Rn", dat.R)
-    # print("Pn", dat.P)
-
     par =  dat.param
-    v = dat.P/par.M
-    F1 = dat.F1 * 1.0
+    F_on_p = dat.F_on_p * 1.0
+    F_on_q = dat.F_on_q * 1.0
 
     # half electronic evolution
     dat.ci = dat.ci * np.exp(-1j*dt*dat.E/2.0)
     cD =  dat.U @ dat.ci # to diabatic basis
     # ======= Nuclear Block =================================
-    dat.R += v * dt + 0.5 * F1 * dt ** 2 / par.M
-    
+
+    dat.P -= (dat.ω**2 * dat.R + F_on_p) * (dt/2.0)
+    dat.R += (dat.P + F_on_q) * dt
+    dat.P -= (dat.ω**2 * dat.R + F_on_p) * (dt/2.0)
+
     #------ Do QM ----------------
-    dat.Hij  = par.Hel(dat.R) + 0j
-    dat.dHij = par.dHel(dat.R)
-    dat.dH0  = par.dHel0(dat.R)
+    dat.Hij  = par.Hel(dat.R,dat.P) + 0j
+    dat.dHij_dp = par.dHel_dp(dat.R,dat.P)
+    dat.dHij_dq = par.dHel_dq(dat.R,dat.P)
+    #dat.dH0  = par.dHel0(dat.R,dat.P)
     #-----------------------------
     dat.E, dat.U = np.linalg.eigh(dat.Hij) 
-    F2 = Force(dat.dHij, dat.dH0, acst, dat.U) # force at t2
-    v += 0.5 * (F1 + F2) * dt / par.M
-    dat.F1 = F2 * 1.0
-    dat.P = v * par.M
+    dat.F_on_q = Force_on_q(dat.dHij_dp, acst, dat.U) # force at t2
+    dat.F_on_p = Force_on_p(dat.dHij_dq, acst, dat.U) # force at t2
     # ======================================================
     dat.ci = np.conj(dat.U).T @ cD # back to adiabatic basis
     
@@ -91,45 +91,6 @@ def VelVer(ogdat, acst, dt) :
     dat.ci = dat.ci * np.exp(-1j*dt*dat.E/2.0)
 
     return dat
-
-
-# def VelVer(ogdat, acst, dt) : 
-
-#     dat = copy.deepcopy(ogdat)
-
-#     print("Rn", dat.R)
-#     print("Pn", dat.P)
-
-#     par =  dat.param
-#     F1 = dat.F1 * 1.0
-
-#     # half electronic evolution
-#     dat.ci = dat.ci * np.exp(-1j*dt*dat.E/2.0)
-#     cD =  dat.U @ dat.ci # to diabatic basis
-#     # ======= Nuclear Block =================================
-#     dat.P += (-dat.dH0 + F1) * (dt)
-#     dat.R += dat.P * dt
-#     print('F1:',F1)
-#     print('U',dat.U)
-#     print('E',dat.E)
-    
-#     #------ Do QM ----------------
-#     dat.Hij  = par.Hel(dat.R) + 0j
-#     dat.dHij = par.dHel(dat.R)
-#     dat.dH0  = par.dHel0(dat.R)
-#     #-----------------------------
-#     dat.E, dat.U = np.linalg.eigh(dat.Hij) 
-#     print("E", dat.E)
-#     F2 = Force(dat.dHij, dat.dH0, acst, dat.U) # force at t2
-#     #dat.P += F2 * (dt/2)
-#     dat.F1 = F2 * 1.0
-#     # ======================================================
-#     dat.ci = np.conj(dat.U).T @ cD # back to adiabatic basis
-    
-#     # half electronic evolution
-#     dat.ci = dat.ci * np.exp(-1j*dt*dat.E/2.0)
-
-#     return dat
 
 def pop(c): # returns the density matrix estimator (populations and coherences)
     NStates = len(c)
@@ -139,12 +100,10 @@ def pop(c): # returns the density matrix estimator (populations and coherences)
     prod = np.outer(c,np.conj(c)) 
     return alpha * prod + beta * np.identity(NStates) # works in any basis
 
-def corr (ci, k, Nstates, initState):
+def corr (ck, k, Nstates, initState):
     sumN = np.sum(np.array([1/n for n in range(1,Nstates+1)])) # constant based on NStates
     alpha = (Nstates - 1)/(sumN - 1) # magnitude scaling
     #print('alpha=',alpha)
-    ## this ci is a diabat in real space 
-    ck = np.einsum('kn,n->k',np.exp(np.outer(1j*k,np.arange(Nstates))),ci)/np.sqrt(Nstates)
     return alpha*ck[initState]
 
 def checkHop(acst, c): # calculate current active state and store result
@@ -154,12 +113,29 @@ def checkHop(acst, c): # calculate current active state and store result
         return True, acst, n_max
     return False, acst, acst
 
+def γ_ab (dat, a, b, δ_p, δ_q):
+    δ = np.cos(dat.k*dat.n) * δ_q - dat.ω * np.sin(dat.k*dat.n) * δ_p
+    
+    k = dat.k
+    Pn = np.zeros(dat.NStates)
+    for i in range(dat.NStates):
+        Pn[i] = (dat.ω/np.sqrt(dat.NStates))*np.sum(-np.sin(k*i)*dat.R+np.cos(k*i)*dat.P/dat.ω)
+
+    a_ = np.sum((δ/np.linalg.norm(δ))**2)/2
+    b_ = -1*np.sum(δ*Pn)/np.linalg.norm(δ)
+    c_ = dat.E[b] - dat.E[a]
+
+    if b_ <= 0:
+        return (-b_ - np.sqrt(b_**2 - 4*a_*c_))/(2*a_), δ, Pn
+    else:
+        return (-b_ + np.sqrt(b_**2 - 4*a_*c_))/(2*a_), δ, Pn
 
 def hop(dat, a, b):
     
     if a != b:
         # a is previous active state, b is current active state
-        P = dat.P/np.sqrt(dat.param.M) # momentum rescaled
+        P = dat.P # momentum rescaled
+        R = dat.R
         ΔE = np.real(dat.E[b] - dat.E[a]) 
         
         if (np.abs(ΔE) < 1E-13):
@@ -170,6 +146,7 @@ def hop(dat, a, b):
         
         Ψa = dat.U[:,a]
         Ψb = dat.U[:,b]
+
         
         # # direction -> 1/√m ∑f Re (c[f] d [f,a] c[a] - c[f] d [f,b] c[b])  # c[f] = ∑m <m | Ψf> 
         # #            =Re ( 1/√m ∑f ∑nm Ψ[m ,f]^ . (<m | dH/dRk | n> ) . Ψ[n ,a] /(E[a]-E[f])
@@ -184,38 +161,45 @@ def hop(dat, a, b):
         fma = np.einsum('f, mf, f -> m', cad, dat.U.conjugate(), rΔEa)
         fmb = np.einsum('f, mf, f -> m', cad, dat.U.conjugate(), rΔEb)
         
-        term1 = np.einsum('n, mnk, k -> m', fma, dat.dHij, Ψa * cad[a].conjugate())
-        term2 = np.einsum('n, mnk, k -> m', fmb, dat.dHij, Ψb * cad[b].conjugate())
+        term1_p = np.einsum('n, mnk, k -> m', fma, dat.dHij_dp, Ψa * cad[a].conjugate())
+        term2_p = np.einsum('n, mnk, k -> m', fmb, dat.dHij_dp, Ψb * cad[b].conjugate())
+
+        term1_q = np.einsum('n, mnk, k -> m', fma, dat.dHij_dq, Ψa * cad[a].conjugate())
+        term2_q = np.einsum('n, mnk, k -> m', fmb, dat.dHij_dq, Ψb * cad[b].conjugate())
         
-        δk = (term1 - term2).real * 1/np.sqrt(dat.param.M) 
+        δk_p = (term1_p - term2_p).real 
+        δk_q = (term1_q - term2_q).real
 
-        #Project the momentum to the new direction
-        P_proj = np.dot(P,δk) * δk / np.dot(δk, δk) if np.dot(P,δk) != 0.0 else P * 0.0
+        # information from real space rescaling
+        γ, δ, Pn = γ_ab(dat, a, b, δk_p, δk_q)
 
-        #print('np.dot(δk, δk) ',np.dot(δk, δk) )
-        #Compute the orthogonal momentum
-        P_orth = P - P_proj # orthogonal P
+        #Project the momentum in real space to the new direction
+        Pn_proj = np.dot(Pn,δ) * δ / np.dot(δ, δ) if np.dot(Pn,δ) != 0.0 else P * 0.0
+
+        # #Project the momentum in kspace to the new direction
+        # Pk_proj = np.dot(P,δ) * δ / np.dot(δ, δ) if np.dot(P,δ) != 0.0 else P * 0.0
+
+        # #Project the position in kspace to the new direction
+        # Rk_proj = np.dot(R,δ) * δ / np.dot(δ, δ) if np.dot(R,δ) != 0.0 else R * 0.0
 
         #Compute projected norm, which will be useful later
-        P_proj_norm = np.sqrt(np.dot(P_proj,P_proj))
+        Pn_proj_norm = np.sqrt(np.dot(Pn_proj,Pn_proj))
         
-        #Compute the total kinetic energy in the projected direction
-
-        if(P_proj_norm**2 < 2*ΔE): # rejected hop
-            print("Rejected Hop")
-            P_proj = -P_proj # reverse projected momentum
-            P = P_orth + P_proj
+        if(Pn_proj_norm**2 < 2*ΔE): # rejected hop
+            print('rejected hop')
+            g = 2*np.dot(Pn,δ)
+            P -= g * δk_q / np.dot(δ, δ)
+            R += g * δk_p / np.dot(δ, δ)
             accepted = False
         else: # accepted hop
-            print("Accepted Hop")
-            P_proj = np.sqrt(P_proj_norm**2 - 2*ΔE)/P_proj_norm * P_proj if P_proj_norm != 0 else P * 0.0 #re-scale the projected momentum
-            P = P_orth + P_proj
+            print('accepted hop')
+            R = R + γ * δk_p / np.linalg.norm(δ)
+            P = P - γ * δk_q / np.linalg.norm(δ)
             accepted = True
-        P *= np.sqrt(dat.param.M)
-        
         dat.P = P.real
-        return P.real, accepted
-    return dat.P, False
+        dat.R = R.real
+        return R.real, P.real, accepted
+    return dat.R, dat.P, False
 
 def runTraj(parameters):
     #------- Seed --------------------
@@ -231,7 +215,9 @@ def runTraj(parameters):
     initState = parameters.initState # intial state
     nskip = parameters.nskip
     dtN   = parameters.dtN
-    k = np.arange(0,2*np.pi,2*np.pi/(NStates))
+    k = parameters.k
+    n = np.arange( NStates )
+    ω = parameters.w
     #---------------------------
     if NSteps%nskip == 0:
         pl = 0
@@ -250,27 +236,28 @@ def runTraj(parameters):
 
 
         #----- Initial QM --------
-        dat.Hij  = parameters.Hel(dat.R)
-        dat.dHij = parameters.dHel(dat.R)
-        dat.dH0  = parameters.dHel0(dat.R)
-        
+        dat.Hij  = parameters.Hel(dat.R,dat.P)
+        dat.dHij_dp = parameters.dHel_dp(dat.R,dat.P)
+        dat.dHij_dq = parameters.dHel_dq(dat.R,dat.P)
+        #dat.dH0  = parameters.dHel0(dat.R,dat.P)
+        dat.k = k
+        dat.n = n
+        dat.ω = ω
+        dat.NStates = NStates
         
         # Call function to initialize mapping variables
-        dat.ci = initElectronic(NStates, initState, dat.Hij) # np.array([0,1])
+        dat.ci = initElectronic(NStates, initState, dat.Hij, k) # np.array([0,1])
         acst = np.argmax(np.abs(dat.ci))
         dat.E, dat.U = np.linalg.eigh(dat.Hij) 
-        dat.F1 = Force(dat.dHij, dat.dH0, acst, dat.U) # Initial Force
+        dat.F_on_q = Force_on_q(dat.dHij_dp, acst, dat.U) # Initial Force
+        dat.F_on_p = Force_on_q(dat.dHij_dq, acst, dat.U) # Initial Force
         #----------------------------
         iskip = 0 # please modify
         t0 = time.time()
         for i in range(NSteps): # One trajectory
-            # eph = 0.5 * np.sum(dat.P**2 / parameters.M + (parameters.w * dat.R)**2 )
-            
-            # if i==0:
-            #     np.savetxt("E_traj_mash.txt", np.column_stack( (dtN*i,dat.E[0],dat.E[1],dat.E[acst],eph) ) )
-            # else:
-            #     with open("E_traj_mash.txt", "a") as f:
-            #         np.savetxt(f, np.column_stack( (dtN*i,dat.E[0],dat.E[1],dat.E[acst],eph) ) )
+            print('step:',i)
+            # print('R:',dat.R)
+            # print('P:',dat.P)
             #------- ESTIMATORS-------------------------------------
             if (i % nskip == 0):
                 
@@ -282,7 +269,7 @@ def runTraj(parameters):
             maxhop = 10
             
             #if(checkHop(acst, dat0.ci)[0]==True):
-            if (hop(dat0, acst, checkHop(acst, dat0.ci)[2])[1]): 
+            if (hop(dat0, acst, checkHop(acst, dat0.ci)[2])[-1]): 
                 newacst = checkHop(acst, dat0.ci)[2]
                 # lets find the bisecting point
                 tL, tR = 0, dtN
@@ -294,8 +281,9 @@ def runTraj(parameters):
                     else:
                         tR = tm
                 
-                P, accepted = hop(dat_tm, acst, newacst)
+                R, P, accepted = hop(dat_tm, acst, newacst)
                 if accepted:
+                    dat_tm.R = R # position update
                     dat_tm.P = P # momentum update
                     acst = newacst
                     
